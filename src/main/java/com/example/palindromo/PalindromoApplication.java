2368f85b3b28c0a09238c06c460cc0de8bdd22f1
gabrielluz <gabriel-h-luz@hotmail.com>
2022-04-19 15:07:19 -0300

    tratamento de acentos

--- src/main/java/com/example/palindromo/PalindromoApplication.java
+++ src/main/java/com/example/palindromo/PalindromoApplication.java
@@ -1,11 +1,9 @@
 package com.example.palindromo;
 
-import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.SystemPropertyUtils;
 
-import java.util.*;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PalindromoApplication {
@@ -18,15 +16,18 @@ public class PalindromoApplication {
 		System.out.println("Insira a string para a verificação: ");
 
 		String entrada = scanner.nextLine();
-		String palavra = entrada.replace(" ","").replaceAll("\\p{Punct}", "").toLowerCase();
+		String palavra = entrada.replaceAll(" ","").replaceAll("\\p{Punct}", "")
+				.toLowerCase()
+				.replaceAll("á|à|â|ã|ä", "a")
+				.replaceAll("é|è|ê|ë", "e").replaceAll("ì|í|î|ï", "i")
+				.replaceAll("ò|ó|ô|õ|ö", "o").replaceAll("ù|ú|û|ü", "u");
+
 		int j = palavra.length() - 1;
 
-		System.out.println(palavra);
+		//System.out.println(palavra);
 
 		for(int i=0; i<j;i++, j--){
-			if (palavra.charAt(i)==palavra.charAt(j)){
-
-			}else{
+			if (palavra.charAt(i)!=palavra.charAt(j)){
 				System.out.println("Não é palíndroma");
 				System.exit(0);
 			}
